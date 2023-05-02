4db457f295c7c49976c2bbb96879bfdb221a1229
Francisco Minen <fran.minen@gmail.com>
2023-05-02 16:56:11 -0300

    Add cron-job.org

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -1,13 +1,20 @@
-import middleware from './middleware'
 import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next'
 import api from './api'
 
 const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+    const cronJobSecret = process.env.NEXT_PUBLIC_CRONJOB_AUTH_TOKEN;
+
+    if (req.headers['cron-job'] !== cronJobSecret) {
+        res.status(401).send('Acceso no autorizado');
+        return;
+    }
+
     const datos = await api.setFiveRandomTeams();
 
     res.status(200)
         .setHeader('Clear-Storage', 'true')
+        .setHeader('Cron-Job', `${cronJobSecret}`)
         .json(datos);
 }
 
-export default handler
+export default handler;
