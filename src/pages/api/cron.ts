2676114652078d6bfd610d39ab159dccb329df69
Francisco Minen <fran.minen@gmail.com>
2023-05-02 14:14:04 -0300

    Hot fix cronjob

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -3,10 +3,10 @@ import api from "./api";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     // Verificar la presencia y el valor del parámetro de consulta compartido
-    if (req.query.key !== process.env.NEXT_PUBLIC_CRONJOB_KEY) {
+    /* if (req.query.key !== process.env.NEXT_PUBLIC_CRONJOB_KEY) {
         res.status(404).end();
         return;
-    }
+    } */
 
     const datos = await api.setFiveRandomTeams();
 
